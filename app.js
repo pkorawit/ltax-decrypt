47f31c2af8d7d25fa3fb2ba141ec4a9b9ea30170
Korawit Prutsachainimmit <korawit.p@gmail.com>
2021-03-24 21:22:38 +0700

    PacelCode decrypt demo

---
+++ app.js
@@ -0,0 +1,22 @@
+const { exec } = require("child_process");
+
+const parcelCodes = ["10A001", "10A002", "10A003", "10A004", "10A005", "10A006"]
+
+for(code in parcelCodes){
+
+    parcelCode = parcelCodes[code]
+
+    exec(`.\\utility\\LTax4Utility.exe ${parcelCode}`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`${parcelCode} => ${stdout}`);
+    });
+
+}
+
