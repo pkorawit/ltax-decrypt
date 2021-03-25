ece668cfe60b38643bc79630cb7f648b0dc0d2ad
Korawit Prutsachainimmit <korawit.p@gmail.com>
2021-03-25 22:37:36 +0700

    build utility as .net core 2.1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const parcelCodes = ["10A001", "10A002", "10A003", "10A004", "10A005", "10A006"]
 
 for(code in parcelCodes){
 
-    parcelCode = parcelCodes[code]
+    let parcelCode = parcelCodes[code]
 
     exec(`.\\utility\\LTax4Utility.exe ${parcelCode}`, (error, stdout, stderr) => {
         if (error) {
